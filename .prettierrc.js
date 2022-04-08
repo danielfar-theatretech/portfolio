9bc790924defe88bd22d37c3c8d682dd1f8d6b48
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2022-04-08 01:01:46 +0100

    boilerplate stuffs

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 4,
+	printWidth: 140,
+	semi: true,
+	singleQuote: false,
+	trailingComma: "none",
+	bracketSpacing: true,
+	arrowParens: "always",
+	embeddedLanguageFormatting: "auto"
+};
